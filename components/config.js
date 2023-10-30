bab42874cac96138ad6e9834496111d0b21c351a
goonadutch <goonadutch@gmail.com>
2023-10-30 09:21:30 +0530

    Added some links

--- components/config.js
+++ components/config.js
@@ -2,3 +2,17 @@ export let authorName = "Neha Mailvakanan"
 export let siteTitle = `That Blog by ${authorName}`
 export let aboutAuthor = "Full Stack Web Developer"
 export let blogsAbout = "Some Projects I've Worked On"
+export let topBarLinks = [
+    {
+        title: "Github",
+        url: "https://github.com/goonadutch"
+    },
+    {
+        title: "LinkedIn",
+        url: "https://linkedin.com/in/neha-mailvakanan"
+    },
+    {
+        title: "Email",
+        url: "mailto:thisisrealneha@gmail.com"
+    }
+]
